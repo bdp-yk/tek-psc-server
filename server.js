336f02631a8ec96895c33613b4d94531642551a8
BigDaddyPanda <yassinkisrawi42@gmail.com>
2019-07-18 00:54:41 +0100

    Updated start script2

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(express.static("app/public"));
-const PORT = process.env.port || 5000;
 app.get("/", (req, res) => {
   res.json("Hello World");
 });
 require("./app/cfStandings")(app);
 require("./app/cfSubmissions")(app);
-app.listen(PORT, () => console.log("App listening on port", PORT));
+app.listen();
